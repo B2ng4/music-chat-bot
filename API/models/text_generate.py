d5524463a64e35140f080ec41d7797c73a149f2d
TimofeyProjects <101704774+tim26006@users.noreply.github.com>
2024-11-06 13:11:43 +1000

    add text clean

--- API/models/text_generate.py
+++ API/models/text_generate.py
@@ -1,12 +1,54 @@
 
 import os
+import asyncio
 from gigachat import GigaChat
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 GIGACHAT_TOKEN = os.getenv("GIGACHAT_TOKEN")
+
+
+def clean_text(text: str) -> str:
+    """
+         Функция для очистки текста песни от лишних элементов
+        input:  text (str): Исходный текст песни
+        output:  str: Очищенный текст песни
+        """
+    # Список слов для удаления
+    words_to_remove = ['Конечно, вот примерный текст для трех куплетов:', 'Надеюсь, этот текст поможет вам завершить вашу песню!', '---']
+
+    # Разбиваем текст на строки
+    lines = text.split('\n')
+
+    # Очищаем строки
+    cleaned_lines = []
+    for line in lines:
+        # Убираем начальные и конечные пробелы
+        line = line.strip()
+
+        # Пропускаем пустые строки
+        if not line:
+            continue
+
+        # Пропускаем строки, содержащие слова из списка words_to_remove
+        skip_line = False
+        for word in words_to_remove:
+            if word in line:
+                skip_line = True
+                break
+        if skip_line:
+            continue
+
+        # Убираем звездочки
+        line = line.replace('*', '')
+
+        # Добавляем очищенную строку
+        cleaned_lines.append(line)
+
+    # Соединяем строки обратно с переносом строки
+    return '\n'.join(cleaned_lines)
+
+
 async def music_text_generate(prompt:str)->str:
     """
     Функция для написания текста песни с помощью  нейросети Gigachat
@@ -20,7 +62,16 @@ async def music_text_generate(prompt:str)->str:
     response = giga.chat(
         f"{prompt} -> Это наброски для текста песни, дополни, чтобы получились полноценные слова песни. Должно  быть не менее 3х куплетов!")
     response = response.choices[0].message.content
-    return response
+    return  clean_text(response) #очищаем вывод
 
 
-#(music_text_generate("Дождь, ожидание холод, боль, снова я  расколот"))
+
+#////////////////////////////// ДЛЯ ТЕСТА ///////////////////////////////////////////////
+
+# async def main():
+#     result = await music_text_generate("Дождь, ожидание холод, боль, снова я расколот")
+#     print(result)
+#
+# # Запускаем асинхронную функцию
+# if __name__ == "__main__":
+#     asyncio.run(main())
