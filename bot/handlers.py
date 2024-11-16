8bcdba39e156a7bcc46443db65ded45c4e77fb83
B2ng4 <125038202+B2ng4@users.noreply.github.com>
2024-11-16 19:54:46 +1000

    багафиксы

--- bot/handlers.py
+++ bot/handlers.py
@@ -7,10 +7,14 @@ from keyboards.accept import keyboard_accept
 from keyboards.main import keyboard_main
 
 from models.ModelForm import Form
+from models.ModelText2ImageAPI import Text2ImageAPI
 
 from apiRequests.generateVocal import generateVocal
 from apiRequests.generateText import generateText
 
+from modules.generateImage import generateImage
+
+import os
 
 router = Router()
 
@@ -22,14 +26,33 @@ async def start_handler(msg: Message):
 @router.message(lambda message: message.text == "У меня уже есть текст 🎶")
 async def request_text(msg: Message, state: FSMContext):
     await msg.answer("Отправьте мне свой текст")
-
     await state.set_state(Form.waiting_for_text)
 
 @router.message(Form.waiting_for_text)
 async def generate(msg: Message, state: FSMContext):
+    user_text = msg.text
+    await state.update_data(user_text=user_text)
+    await msg.answer("Отправьте промт 📜\n \n(Например: Мужчина с бунтарским, резким духом в стиле панк, с агрессивным тоном)", parse_mode="Markdown")
+    await state.set_state(Form.waiting_for_promt)
+
+
+@router.message(Form.waiting_for_promt)
+async def generate_promt(msg: Message, state: FSMContext):
+    user_data = await state.get_data()
+    user_text = user_data.get('user_text')
+    promt = msg.text
+    await msg.answer("Получил промт 👌", parse_mode="Markdown")
     await msg.answer("⚙️⚙️⚙️*Погодите*...*Я сочиняю*...⚙️⚙️️⚙️", parse_mode="Markdown")
-    voice_file = generateVocal(msg.text)
-    await msg.answer_voice(voice=FSInputFile(voice_file),caption="Сгенерированный вокал")
+    voice_file = generateVocal(user_text)
+    api_kandinsky = Text2ImageAPI('https://api-key.fusionbrain.ai/',
+                                  api_key=os.getenv("api_key"),
+                                  secret_key=os.getenv("secret_key"))
+    model_id = api_kandinsky.get_model()
+    prompt = api_kandinsky.generate("Cоздай обложку для песни, её первые слова:", model_id)
+    generated_images = api_kandinsky.check_generation(prompt)
+    photos = await generateImage(generated_images, msg.from_user.username)
+    await msg.answer_voice(voice=FSInputFile(voice_file),caption="Сгенерированный вокал", reply_markup=keyboard_main)
+    await msg.answer_photo(photo=photos[0], caption="Вот ваша обложка")
     await state.clear()
 
 @router.message(lambda message: message.text == "Генерация с нуля 🎹")
@@ -37,15 +60,9 @@ async def request_text(msg: Message):
     await msg.answer("Напишите, о чем должен быть текст?", parse_mode="Markdown")
     @router.message()
     async def receive_text(msg: Message):
+        await msg.answer("⚙️⚙️⚙️*Погодите*...*Сочиняю*...⚙️⚙️⚙️", parse_mode="Markdown")
         text = msg.text
         genText = f'{generateText(text)}'
-        # api_kandinsky = Text2ImageAPI('https://api-key.fusionbrain.ai/',
-        #                               api_key=os.getenv("api_key"),
-        #                               secret_key=os.getenv("secret_key"))
-        # model_id = api_kandinsky.get_model()
-        # prompt = api_kandinsky.generate("Cоздай обложку для песни, её первые слова:", model_id)
-        # generated_images = api_kandinsky.check_generation(prompt)
-        # photos = await generateImage(generated_images, msg.from_user.username)
         await msg.answer(genText, reply_markup=keyboard_accept)
 
 @router.message(lambda message: message.text == "Нет! Перегенерируй ⛔")
--- bot/models/ModelForm.py
+++ bot/models/ModelForm.py
@@ -2,3 +2,4 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Form(StatesGroup):
     waiting_for_text = State()
+    waiting_for_promt = State()
